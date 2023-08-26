45899b42f4adaf774ef1fff377cc73debf1d6ee7
1MioSubs <it09022022@gmail.com>
2023-08-26 12:48:17 +0300

    added new module v0.6

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const concat        = require("gulp-concat");
 const autoprefixer  = require("gulp-autoprefixer");
 const uglify        = require("gulp-uglify");
 const imagemin      = require("gulp-imagemin");
+const rename        = require("gulp-rename");
 const clean         = require("gulp-clean");
 const browserSync   = require("browser-sync").create();
 
@@ -13,30 +14,40 @@ function browsersync() {
     server: {
       baseDir: "app/",
     },
-    notofy: false,
+    notify: false,
   })
 }
 
 function styles() {
   return src("app/scss/style.scss")
     .pipe(scss({ outputStyle: "expanded" })) //compressed -  expanded
-    .pipe(concat("style.min.css"))
+    // .pipe(concat("style.min.css"))
+    .pipe(rename({ suffix: ".min"}))
     .pipe(
       autoprefixer({
         overrideBrowserslist: ["last 10 versions"],
-        grid: true,
+        add: true,
+        grid: false,
       })
     )
     .pipe(dest("app/css"))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
 }
 
 function scripts() {
-  return src(["node_modules/jquery/dist/jquery.js", "app/js/main.js"])
+  return src([
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
+    "node_modules/jquery-form-styler/dist/jquery.formstyler.js",
+    "node_modules/slick-carousel/slick/slick.js",
+    "node_modules/rateyo/src/jquery.rateyo.js",
+    "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
+    "app/js/main.js",
+  ])
     .pipe(concat("main.min.js"))
     .pipe(uglify())
     .pipe(dest("app/js"))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
   }
   
   function cleadDist() {
